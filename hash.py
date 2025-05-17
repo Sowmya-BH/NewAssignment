3df610f282738f5ec1ff2ff74d0732d1ac624b1b
Sowmya-BH <60283108+Sowmya-BH@users.noreply.github.com>
2025-05-18 00:59:13 +0530

    Delete hash.py

--- hash.py
+++
@@ -1,8 +0,0 @@
-# import streamlit_authenticator as stauth
-
-# hashed_password = stauth.Hasher().generate(['plantoai'])
-# print(hashed_password)
-
-from streamlit_authenticator.utilities.hasher import Hasher
-hashed_passwords = Hasher(['plantoai']).generate()
-print(hashed_passwords)
